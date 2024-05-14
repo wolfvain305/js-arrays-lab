91062006bba98ea986edec46ed341df04ce79e37
wolfvain305 <dwolf0305@gmail.com>
2024-05-14 19:09:20 -0400

    exercise 6

--- app.js
+++ app.js
@@ -24,3 +24,55 @@ foods.push('pizza')
 foods.push('cheeseburger')
 
 console.log('Exercise 2 result:', foods);
+
+/*
+Exercise 3: Insert at the beginning
+
+1) Insert the string 'taco' at the beginning of the `foods` array.
+
+Complete Exercise 3 in the space below:
+*/
+foods.unshift('taco')
+
+
+console.log('Exercise 3 result:', foods);
+
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+const favFood = foods[1]
+
+
+console.log('Exercise 4 result:', favFood);
+
+/*
+Exercise 5: Insert an element between two others
+
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
+
+Complete Exercise 5 in the space below:
+*/
+
+foods.splice(2, 0, 'tofu')
+
+console.log('Exercise 5 result:', foods);
+
+/*
+Exercise 6: Replace elements
+
+1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
+
+Complete Exercise 6 in the space below:
+*/
+
+foods.splice(1,1, 'sushi', 'cupcake')
+
+console.log('Exercise 6 result:', foods);
+
