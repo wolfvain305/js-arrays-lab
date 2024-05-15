396185fc47c525fc4beef9b5d51675f287d054b0
wolfvain305 <dwolf0305@gmail.com>
2024-05-14 20:07:47 -0400

    exercise 10

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ Exercise 7: Using the `slice()` method
 Complete Exercise 7 in the space below:
 */
 
-const yummy = foods.splice(-4,2)
+const yummy = foods.slice(1,2)
 
 console.log('Exercise 7 result:', yummy);
 
@@ -106,3 +106,36 @@ const soyIdx = foods.indexOf('tofu')
 
 
 console.log('Exercise 8 result:', soyIdx);
+
+/*
+Exercise 9: Joining elements
+
+1) Use the `join()` method to concatenate the strings in the `foods` array, 
+   separated by ' -> '. 
+
+2) Assign the result to a variable called `allFoods`. 
+
+Note: The final result should log as:
+'taco -> sushi -> cupcake -> tofu -> cheeseburger'
+
+Complete Exercise 9 in the space below:
+*/
+
+const allFoods = foods.join(' -> ')
+
+console.log('Exercise 9 result:', allFoods);
+
+/*
+Exercise 10: Check for an element
+
+1) Using the .includes() method, check if the `foods` array contains the string
+   'soup'.
+
+2) Assign the result to a variable called `hasSoup``.
+
+Complete Exercise 10 in the space below:
+*/
+
+const hasSoup = foods.includes('soup')
+
+console.log('Exercise 10 result:', hasSoup);
